3f3168ac37b55becda9cda23ae76f4e6df9afbc1
Ashley Bland <ashleyjbland@gmail.com>
2019-07-14 17:54:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,11 +10,16 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/puppy' do
-    @name = params[:name]
-    @breed = params[:breed]
-    @age = params[:months_old]
+  #post '/puppy' do
+  #  @name = params[:name]
+  #  @breed = params[:breed]
+  #  @age = params[:months_old]
 
+  #  erb :display_puppy
+#  end
+
+  post '/puppy' do
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
   end
 
